875d9d7a728016b3da6c2f3151cb4aae8cbc4b85
JYP's 매크부크 <lineplus@LinePlusui-MacBook-Pro-2.local>
2016-01-26 10:33:25 +0900

    토비 스프링 1.2.2

---
+++ jyp/src/main/java/springbook/user/dao/UserDao.java
@@ -0,0 +1,64 @@
+package springbook.user.dao;
+
+import springbook.user.domain.User;
+import sun.tools.java.ClassNotFound;
+
+import java.sql.*;
+
+public class UserDao {
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost/springbook", "root", "root");
+    }
+
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Connection c = getConnection();
+
+        PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values (?,?,?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Connection c = getConnection();
+
+        PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+
+    public static void main(String args[]) throws SQLException, ClassNotFoundException {
+        System.out.println("dd");
+
+        UserDao userDao = new UserDao();
+
+        User user = new User();
+        user.setId("jyp2");
+        user.setName("jyp2");
+        user.setPassword("kaka");
+
+        userDao.add(user);
+
+        System.out.println(userDao.get(user.getId()));
+    }
+
+}

b19145e18f22ab63b55a3586c41b933862ec885c
YoonSung <lvev9925@naver.com>
2016-02-29 00:24:56 +0900

    (Step5) 과제 업로드

---
+++ yoonsung/src/main/java/step5/PropertyValue.java
@@ -0,0 +1,20 @@
+package step5;
+
+public class PropertyValue {
+
+    private String name;
+    private Object value;
+
+    public PropertyValue(String name, Object value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+}

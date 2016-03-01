ac9f4a19e1ce6460ef84477db7cc3bcceb2e7be2
jinyoung.park89 <iamapark89@gmail.com>
2016-03-01 19:15:42 +0900

    InitializingBean 적용

---
+++ jyp/src/main/java/org/springframework/beans/factory/InitializingBean.java
@@ -0,0 +1,9 @@
+package org.springframework.beans.factory;
+
+/**
+ * @author jinyoung.park89
+ * @since 2016-03-01
+ */
+public interface InitializingBean {
+    void afterPropertiesSet() throws Exception;
+}

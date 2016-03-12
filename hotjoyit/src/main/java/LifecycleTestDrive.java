65babcfb2cb36a628a52dec915e78f5773884a29
hotjoyit <hotjoyit@gmail.com>
2016-03-12 22:54:57 +0900

    Step2 :  *Aware lifecycle interface 적용
    * 공통 빈에 일부 *Aware 인터페이스 적용 / 구현
    * 빈이 인스턴스화 된 이후 *Aware 구현 코드가 동작함

--- hotjoyit/src/main/java/LifecycleTestDrive.java
+++ hotjoyit/src/main/java/LifecycleTestDrive.java
@@ -4,7 +4,32 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by hotjoyit on 16. 3. 12.
+ *
+ * <p>Bean factory implementations should support the standard bean lifecycle interfaces
+ * as far as possible. The full set of initialization methods and their standard order is:<br>
+ * 1. BeanNameAware's {@code setBeanName}<br>
+ * 2. BeanClassLoaderAware's {@code setBeanClassLoader}<br>
+ * 3. BeanFactoryAware's {@code setBeanFactory}<br>
+ * 4. ResourceLoaderAware's {@code setResourceLoader}
+ * (only applicable when running in an application context)<br>
+ * 5. ApplicationEventPublisherAware's {@code setApplicationEventPublisher}
+ * (only applicable when running in an application context)<br>
+ * 6. MessageSourceAware's {@code setMessageSource}
+ * (only applicable when running in an application context)<br>
+ * 7. ApplicationContextAware's {@code setApplicationContext}
+ * (only applicable when running in an application context)<br>
+ * 8. ServletContextAware's {@code setServletContext}
+ * (only applicable when running in a web application context)<br>
+ * 9. {@code postProcessBeforeInitialization} methods of BeanPostProcessors<br>
+ * 10. InitializingBean's {@code afterPropertiesSet}<br>
+ * 11. a custom init-method definition<br>
+ * 12. {@code postProcessAfterInitialization} methods of BeanPostProcessors
+ *
+ * <p>On shutdown of a bean factory, the following lifecycle methods apply:<br>
+ * 1. DisposableBean's {@code destroy}<br>
+ * 2. a custom destroy-method definition
  */
+
 public class LifecycleTestDrive {
 
   public static void main(String[] args) {
--- hotjoyit/src/main/java/bean/CommonBean.java
+++ hotjoyit/src/main/java/bean/CommonBean.java
@@ -1,9 +1,18 @@
 package bean;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.core.io.ResourceLoader;
+
 /**
  * Created by hotjoyit on 16. 3. 12.
  */
-public abstract class CommonBean {
+public abstract class CommonBean implements BeanNameAware, BeanClassLoaderAware,
+    BeanFactoryAware, ResourceLoaderAware {
 
   public CommonBean() {
     System.out.println(String.format("@Construct : %s instantiated", getName()));
@@ -21,4 +30,24 @@ public abstract class CommonBean {
   }
 
   public abstract String getName();
+
+  @Override
+  public void setBeanName(String name) {
+    System.out.println(String.format("@BeanNameAware : %s setBeanName() is called", getName()));
+  }
+
+  @Override
+  public void setBeanClassLoader(ClassLoader classLoader) {
+    System.out.println(String.format("@BeanClassLoaderAware : %s setBeanClassLoader() is called", getName()));
+  }
+
+  @Override
+  public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+    System.out.println(String.format("@BeanFactoryAware : %s setBeanFactory() is called", getName()));
+  }
+
+  @Override
+  public void setResourceLoader(ResourceLoader resourceLoader) {
+    System.out.println(String.format("@ResourceLoaderAware : %s setResourceLoader() is called", getName()));
+  }
 }

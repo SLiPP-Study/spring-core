a0e352d4078da0063475153848d21e5869268dfa
조우진 <woojin@tmon.co.kr>
2016-04-20 21:12:13 +0900

    [week6] autowired걸면 prototype이 의미 없으므로 getBean으로 호출

--- week6/src/main/java/net/slipp/week6/service/MemberService.java
+++ week6/src/main/java/net/slipp/week6/service/MemberService.java
@@ -2,10 +2,11 @@ package net.slipp.week6.service;
 
 import net.slipp.week6.beans.Member;
 import net.slipp.week6.beans.Outlier;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,53 +14,41 @@ import java.util.Map;
  * Created by woojin on 2016. 4. 20..
  */
 @Service
-public class MemberService {
-	@Autowired
-	private Member currentMember;
+public class MemberService implements ApplicationContextAware {
+	private ApplicationContext applicationContext;
 
-	@Autowired
-	private Member newMember;
-
-	@Autowired
-	private Member memberByRequest;
-
-	@Autowired
-	private Member memberBySession;
-
-	@Autowired
-	private Outlier aloneMember;
-
-	@Resource
-	private Map<String, Object> memberMap;
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
 
 	public enum SCOPE {SINGLETON, PROTOTYPE, REQUEST, SESSION}
 
 	public Map<String, Object> getMemberMapByScopeInNonSingleton() {
 		Map<String, Object> memberMap = new HashMap<String, Object>();
 
-		memberMap.put("singleton", currentMember);
-		memberMap.put("prototype", newMember);
-		memberMap.put("request", memberByRequest);
-		memberMap.put("session", memberBySession);
-		memberMap.put("outlier", aloneMember);
+		memberMap.put("singleton", applicationContext.getBean("currentMember", Member.class));
+		memberMap.put("prototype", applicationContext.getBean("newMember", Member.class));
+		memberMap.put("request", applicationContext.getBean("memberByRequest", Member.class));
+		memberMap.put("session", applicationContext.getBean("memberBySession", Member.class));
+		memberMap.put("outlier", applicationContext.getBean("aloneMember", Outlier.class));
 		return memberMap;
 	}
 
 	public Map<String, Object> getMemberMapByScopesInSingleton() {
-		return memberMap;
+		return applicationContext.getBean("memberMap", Map.class);
 	}
 
 	public Member getMember(SCOPE scope) {
 		switch (scope) {
 			case PROTOTYPE:
-				return newMember;
+				return 	 applicationContext.getBean("newMember", Member.class);
 			case REQUEST:
-				return memberByRequest;
+				return applicationContext.getBean("memberByRequest", Member.class);
 			case SESSION:
-				return memberBySession;
+				return applicationContext.getBean("memberBySession", Member.class);
 			case SINGLETON:
 			default:
-				return currentMember;
+				return applicationContext.getBean("currentMember", Member.class);
 		}
 	}
 
